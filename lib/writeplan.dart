457017d804e1272c4decf4ba60b3a92efc4eb54b
Jang <zn2309@naver.com>
2021-11-16 16:30:27 +0900

    작성페이지 ui 디자인 완
    색상 파레트 선택 구현중

--- lib/writeplan.dart
+++ lib/writeplan.dart
@@ -1,4 +1,6 @@
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/data/todo.dart';
 
@@ -12,8 +14,8 @@ class WritePlan extends StatefulWidget {
 }
 
 class _WritePlanState extends State<WritePlan> {
-  //TextEditingController titleController = TextEditingController();
-  //TextEditingController memoController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
+  TextEditingController memoController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,10 +34,74 @@ class _WritePlanState extends State<WritePlan> {
           itemBuilder: (ctx, idx) {
         if(idx==0){
           return Container(
-            child:Text("제목")
+              margin:EdgeInsets.only(left:25, right:25, top:20),
+            child:Text("제목", style:TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
           );
         } else if(idx==1){
           return Container(
+            margin:EdgeInsets.only(left:25, right:25, bottom:10),
+            child: TextField(
+              controller: titleController,
+            )
+          );
+        } else if(idx==2){
+          return InkWell(child:Container(
+              margin:EdgeInsets.symmetric(vertical:10, horizontal: 25),
+            child : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children:[
+                Text("색상", style:TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                Container(
+                    color:Color(widget.todo.color==0?Colors.grey.value:widget.todo.color),
+                    width:15,
+                    height:15,
+                  margin:EdgeInsets.only(right:20)
+                )
+              ]
+            )
+          ),
+          onTap: (){
+            List<Color> colors = [
+              Color(0xff80d3f4),
+              Color(0xffa794fa),
+              Color(0xfffb91d1),
+              Color(0xfffb8a94),
+              Color(0xfffebd9a),
+              Color(0xff51e29d),
+              Color(0xffffffff),
+            ];
+
+
+          },
+          );
+        } else if(idx==3){
+          return Container(
+              margin:EdgeInsets.symmetric(vertical:10, horizontal: 25),
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:[
+                Text("카테고리",style:TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                Text(widget.todo.category),
+              ]
+            )
+          );
+        } else if(idx==4){
+          return Container(
+            margin:EdgeInsets.only(left:25, right:25, top:20, bottom:10),
+            child:Text("내용",style:TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+          );
+        } else if(idx==5){
+          return Container(
+              margin:EdgeInsets.only(left:25, right:25, bottom:10),
+            child: TextField(
+              controller:memoController,
+              maxLines: 10,
+              minLines: 10,
+              decoration: InputDecoration(
+                border:OutlineInputBorder(borderSide:BorderSide(color:Colors.black))
+              ),
+            )
           );
         }
         return Container();
